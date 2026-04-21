f652b964f27ede2d510fbafc88918b06db45964d
Freja Thoresen <freja.thoresen@alexandra.dk>
2026-04-21 09:47:16 +0200

    Keep debugger alive

--- lua/plugins/dap-python.lua
+++ lua/plugins/dap-python.lua
@@ -51,4 +51,14 @@ return {
       })
     end,
   },
+
+  {
+    "rcarriga/nvim-dap-ui",
+    opts = function(_, opts)
+      local dap = require("dap")
+      dap.listeners.before.event_terminated["dapui_config"] = nil
+      dap.listeners.before.event_exited["dapui_config"] = nil
+      return opts
+    end,
+  },
 }
